e0070567114b08117d0c73761fed9beb10efa8fb
Kiranpal Juss <>
2026-06-12 13:35:49 +0100

    no message

--- cypress/integration/pages/sauceDemoPage.ts
+++ cypress/integration/pages/sauceDemoPage.ts
@@ -1,74 +1,78 @@
 class SauceDemoPage {
   usernameInput () {
-    return cy.get('#user-name');
+    return cy.get('[data-test="username"]');
   }
 
   passwordInput () {
-    return cy.get('#password');
+    return cy.get('[data-test="password"]');
   }
 
   loginButton () {
-    return cy.get('#login-button');
+    return cy.get('[data-test="login-button"]');
   }
 
   pageTitle () {
-    return cy.get('.title');
+    return cy.get('[data-test="title"]');
   }
 
   sortSelect () {
     return cy.get('[data-test="product-sort-container"]');
   }
 
+  activeSortOption () {
+    return cy.get('[data-test="active-option"]');
+  }
+
   itemPrices () {
-    return cy.get('.inventory_item_price');
+    return cy.get('[data-test="inventory-item-price"]');
   }
 
   itemNames () {
-    return cy.get('.inventory_item_name');
+    return cy.get('[data-test="inventory-item-name"]');
   }
 
   inventoryItemByName (itemName: string) {
-    return cy.contains('.inventory_item', itemName);
+    return cy.contains('[data-test="inventory-item"]', itemName);
   }
 
   addToCartButtonInItem (itemName: string) {
-    return this.inventoryItemByName(itemName).contains('button', 'Add to cart');
+    return this.inventoryItemByName(itemName).find('[data-test^="add-to-cart-"]');
   }
 
   shoppingCartLink () {
-    return cy.get('.shopping_cart_link');
+    return cy.get('[data-test="shopping-cart-link"]');
   }
 
   cartItems () {
-    return cy.get('.cart_item .inventory_item_name');
+    return cy.get('[data-test="cart-list"] [data-test="inventory-item-name"]');
   }
 
   cartBadge () {
-    return cy.get('.shopping_cart_badge');
+    return cy.get('[data-test="shopping-cart-badge"]');
   }
 
   continueShoppingButton () {
-    return cy.get('#continue-shopping');
+    return cy.get('[data-test="continue-shopping"]');
   }
 
   checkoutButton () {
-    return cy.get('#checkout');
+    return cy.get('[data-test="checkout"]');
   }
 
   firstNameInput () {
-    return cy.get('#first-name');
+    return cy.get('[data-test="firstName"]');
   }
 
   lastNameInput () {
-    return cy.get('#last-name');
+    return cy.get('[data-test="lastName"]');
   }
 
   postalCodeInput () {
-    return cy.get('#postal-code');
+    return cy.get('[data-test="postalCode"]');
   }
 
   continueCheckoutButton () {
-    return cy.get('#continue');
+    return cy.get('[data-test="continue"]');
   }
 
   errorMessage () {
